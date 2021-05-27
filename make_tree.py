e3e415cadf5110c14fb0313aa21d694e8a65b79d
kiwi-1919 <HIn@zh.email.cn>
2021-05-27 20:32:16 +0800

    test

--- make_tree.py
+++ make_tree.py
@@ -1,10 +1,12 @@
 import tree
 
 if __name__ == '__main__':
-    string = input('reset:1;show_node:2')
+    string = input('reset:1;show_node:2;add_node:3')
     if string == '1':
         tree.reset(root='*.txt')
     elif string == '2':
         tree.show_node(input('node_tag'))
+    elif string == '3':
+        tree.add_node(name=input('node_tag'), parent=input('parent_name'))
     else:
         print('wait for a minute')
