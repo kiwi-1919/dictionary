567987749f0ddbb86e1ef9d94da3ff11a4576e1e
kiwi-1919 <HIn@zh.email.cn>
2021-05-28 18:45:50 +0800

    test

--- dataframe.py
+++ dataframe.py
@@ -11,12 +11,14 @@ def setup():
     for each in li:
         with open(each + '.aw.txt', 'rt', encoding='utf-8') as rf:
             for item in rf.read().split(';'):
+                if not item:
+                    break
                 cur.execute(f"INSERT OR IGNORE INTO aw (word) VALUES ('{item}')")
     connect.commit()
     connect.close()
     connect = sqlite3.connect('.\\df\\st.db')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE st (sents TEXT) PRIMARY KEY NOT NULL')
+    cur.execute('CREATE TABLE st (sents TEXT PRIMARY KEY NOT NULL)')
     for each in li:
         with open(each + '.csv', 'rt', encoding='utf-8') as rf:
             c = csv.reader(rf)
@@ -37,6 +39,8 @@ def setup():
                 words = str()
                 for word in item:
                     words += (word + ';')
+                if not words:
+                    break
                 cur.execute(f"INSERT OR IGNORE INTO sw (wordlist) VALUES ('{words}')")
     connect.commit()
     connect.close()
