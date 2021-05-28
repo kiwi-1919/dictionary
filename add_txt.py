5280a64edb4227a9ec136494eb715d49c9569833
kiwi-1919 <HIn@zh.email.cn>
2021-05-28 17:18:55 +0800

    test

---
+++ add_txt.py
@@ -0,0 +1,20 @@
+import os
+import random
+import tqdm
+
+
+def add():
+    l = list()
+    while True:
+        if input('continue/not:y/o') == 'y':
+            l.append(input('add from where'))
+        else:
+            break
+    try:
+        for each in tqdm.tqdm(l):
+            os.system(
+                f'copy {each} {os.path.join(os.curdir, "txt", str(random.randint(0, 1000000000000000000)) + ".txt")} '
+                f'/-Y')
+    except:
+        print('fail')
+        pass
