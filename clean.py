1e252ddf39c6a419a189649925db1995aac77d88
kiwi-1919 <HIn@zh.email.cn>
2021-05-28 17:04:40 +0800

    test

---
+++ clean.py
@@ -0,0 +1,10 @@
+import os
+import tqdm
+
+
+def remove():
+    for each in tqdm.tqdm(os.listdir('.\\txt')):
+        os.remove(each + '.aw')
+        os.remove(each + '.aw.txt')
+        os.remove(each + '.csv')
+        os.remove('storey_' + each + '.csv')
