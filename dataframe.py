e0379a38a2293432733a62fa6b2c980570f5c77c
kiwi-1919 <HIn@zh.email.cn>
2021-05-28 18:29:23 +0800

    test

--- dataframe.py
+++ dataframe.py
@@ -7,29 +7,29 @@ def setup():
     li = os.listdir('.\\txt')
     connect = sqlite3.connect('.\\df\\aw.db')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE aw (word TEXT);')
+    cur.execute('CREATE TABLE aw (word TEXT)')
     for each in li:
         with open(each + '.aw.txt', 'rt', encoding='utf-8') as rf:
             for item in rf.read().split(';'):
-                cur.execute(f"INSERT INTO aw VALUES ('{item}')")
+                cur.execute(f"INSERT OR IGNORE INTO aw (word) VALUES ('{item}')")
     connect.commit()
     connect.close()
     connect = sqlite3.connect('.\\df\\st.db')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE st (sents TEXT);')
+    cur.execute('CREATE TABLE st (sents TEXT)')
     for each in li:
         with open(each + '.csv', 'rt', encoding='utf-8') as rf:
             c = csv.reader(rf)
             for item in c:
                 try:
-                    cur.execute(f"INSERT INTO st VALUES ('{item[0]}')")
+                    cur.execute(f"INSERT OR IGNORE INTO st (sents) VALUES ('{item[0]}')")
                 except:
                     pass
     connect.commit()
     connect.close()
     connect = sqlite3.connect('.\\df\\sw.db')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE sw (wordlist TEXT);')
+    cur.execute('CREATE TABLE sw (wordlist TEXT)')
     for each in li:
         with open('storey_' + each + '.csv', 'rt', encoding='utf-8') as rf:
             c = csv.reader(rf)
@@ -37,7 +37,7 @@ def setup():
                 words = str()
                 for word in item:
                     words += (word + ';')
-                cur.execute(f"INSERT INTO sw VALUES ('{words}')")
+                cur.execute(f"INSERT OR IGNORE INTO sw (wordlist) VALUES ('{words}')")
     connect.commit()
     connect.close()
     print('congratulations')
