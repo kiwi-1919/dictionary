981cd482352014f22c8fd6416bdd694b4f1042de
kiwi-1919 <HIn@zh.email.cn>
2021-05-28 23:30:49 +0800

    test

--- dataframe.py
+++ dataframe.py
@@ -1,25 +1,34 @@
 import sqlite3
 import os
 import csv
+import tqdm
 
 
 def setup():
     li = os.listdir('.\\txt')
-    connect = sqlite3.connect('.\\df\\aw.db')
+    connect = sqlite3.connect('.\\df\\data.db')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE aw (word TEXT PRIMARY KEY NOT NULL)')
-    for each in li:
+    try:
+        cur.execute('drop table aw')
+        cur.execute('drop table st')
+        cur.execute('drop table sw')
+        connect.commit()
+    except:
+        pass
+    cur.execute('''CREATE TABLE aw
+    (word TEXT PRIMARY KEY NOT NULL);''')
+    connect.commit()
+    for each in tqdm.tqdm(li):
         with open(each + '.aw.txt', 'rt', encoding='utf-8') as rf:
             for item in rf.read().split(';'):
                 if not item:
                     continue
                 cur.execute(f"INSERT OR IGNORE INTO aw (word) VALUES ('{item}')")
     connect.commit()
-    connect.close()
-    connect = sqlite3.connect('.\\df\\st.db')
-    cur = connect.cursor()
-    cur.execute('CREATE TABLE st (sents TEXT PRIMARY KEY NOT NULL)')
-    for each in li:
+    cur.execute('CREATE TABLE st'
+                '(sents TEXT PRIMARY KEY NOT NULL);')
+    connect.commit()
+    for each in tqdm.tqdm(li):
         with open(each + '.csv', 'rt', encoding='utf-8') as rf:
             c = csv.reader(rf)
             for item in c:
@@ -28,11 +37,10 @@ def setup():
                 except:
                     pass
     connect.commit()
-    connect.close()
-    connect = sqlite3.connect('.\\df\\sw.db')
-    cur = connect.cursor()
-    cur.execute('CREATE TABLE sw (wordlist TEXT PRIMARY KEY NOT NULL)')
-    for each in li:
+    cur.execute('CREATE TABLE sw'
+                '(wordlist TEXT PRIMARY KEY NOT NULL);')
+    connect.commit()
+    for each in tqdm.tqdm(li):
         with open('storey_' + each + '.csv', 'rt', encoding='utf-8') as rf:
             c = csv.reader(rf)
             for item in c:
