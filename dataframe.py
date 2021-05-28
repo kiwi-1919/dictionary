f15610e90bd5420c75a09088fce01b22b0745bdf
kiwi-1919 <HIn@zh.email.cn>
2021-05-28 16:21:58 +0800

    test

---
+++ dataframe.py
@@ -0,0 +1,44 @@
+import sqlite3
+import os
+import csv
+
+
+def setup():
+    li = os.listdir('.\\txt')
+    connect = sqlite3.connect('.\\df\\aw.db')
+    cur = connect.cursor()
+    cur.execute('CREATE TABLE aw (word TEXT)')
+    for each in li:
+        with open(each + '.aw.txt', 'rt') as rf:
+            for item in rf.read().split(';'):
+                cur.execute(f"INSERT INTO aw VALUES('{item}')")
+    connect.commit()
+    connect.close()
+    connect = sqlite3.connect('.\\df\\st.db')
+    cur = connect.cursor()
+    cur.execute('CREATE TABLE st (sents TEXT)')
+    for each in li:
+        with open(each + '.csv', 'rt') as rf:
+            c = csv.reader(rf)
+            for item in c:
+                cur.execute(f"INSERT INTO sents VALUES('{item[0]}')")
+    connect.commit()
+    connect.close()
+    connect = sqlite3.connect('.\\df\\sw.db')
+    cur = connect.cursor()
+    cur.execute('CREATE TABLE sw (wordlist TXT)')
+    for each in li:
+        with open('storey_' + each + '.csv', 'rt') as rf:
+            c = csv.reader(rf)
+            for item in c:
+                words = str()
+                for word in item:
+                    words += (word + ';')
+                cur.execute(f"INSERT INTO sw VALUES('{words}')")
+    connect.commit()
+    connect.close()
+    print('congratulations')
+
+
+if __name__ == '__main__':
+    setup()
