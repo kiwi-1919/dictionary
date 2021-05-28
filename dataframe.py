8e095cde9bf23e640c8a67df398ad1fb932e4996
kiwi-1919 <HIn@zh.email.cn>
2021-05-28 18:42:41 +0800

    test

--- dataframe.py
+++ dataframe.py
@@ -7,7 +7,7 @@ def setup():
     li = os.listdir('.\\txt')
     connect = sqlite3.connect('.\\df\\aw.db')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE aw (word TEXT)')
+    cur.execute('CREATE TABLE aw (word TEXT PRIMARY KEY NOT NULL)')
     for each in li:
         with open(each + '.aw.txt', 'rt', encoding='utf-8') as rf:
             for item in rf.read().split(';'):
@@ -16,7 +16,7 @@ def setup():
     connect.close()
     connect = sqlite3.connect('.\\df\\st.db')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE st (sents TEXT)')
+    cur.execute('CREATE TABLE st (sents TEXT) PRIMARY KEY NOT NULL')
     for each in li:
         with open(each + '.csv', 'rt', encoding='utf-8') as rf:
             c = csv.reader(rf)
@@ -29,7 +29,7 @@ def setup():
     connect.close()
     connect = sqlite3.connect('.\\df\\sw.db')
     cur = connect.cursor()
-    cur.execute('CREATE TABLE sw (wordlist TEXT)')
+    cur.execute('CREATE TABLE sw (wordlist TEXT PRIMARY KEY NOT NULL)')
     for each in li:
         with open('storey_' + each + '.csv', 'rt', encoding='utf-8') as rf:
             c = csv.reader(rf)
