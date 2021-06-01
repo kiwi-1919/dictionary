d291b3cada3a536c876549039c1bb5dfbfc716b5
kiwi-1919 <HIn@zh.email.cn>
2021-06-01 19:22:07 +0800

    test

--- add_txt.py
+++ add_txt.py
@@ -1,6 +1,36 @@
 import os
 import random
 import tqdm
+import sqlite3
+import hashlib
+
+
+def check(path=None):
+    if not path:
+        connect = sqlite3.connect('.\\df\\data.db')
+        cur = connect.cursor()
+        try:
+            cur.execute('drop table hash')
+            connect.commit()
+        except:
+            pass
+        cur.execute('CREATE TABLE hash (value TEXT);')
+        connect.commit()
+        for each in tqdm.tqdm(os.listdir('.\\txt')):
+            with open(os.path.join('.\\txt', each), 'rt') as rf:
+                value = hashlib.sha1(rf.read().encode())
+                cur.execute(f'INSERT OR IGNORE INTO hash (value) VALUES ({value})')
+        connect.commit()
+    else:
+        connect = sqlite3.connect('.\\df\\data.db')
+        cur = connect.cursor()
+        with open(path, 'rt') as rf:
+            hash_value = hashlib.sha1(rf.read().encode())
+        cur.execute("PRAGMA table_info(hash)")
+        if (hash_value) in cur.fetchall():
+            pass
+        else:
+            raise Exception('SameFileError')
 
 
 def add():
@@ -10,8 +40,10 @@ def add():
             l.append(input('add from where'))
         else:
             break
+    check()
     try:
         for each in tqdm.tqdm(l):
+            check(path=each)
             os.system(
                 f'copy {each} {os.path.join(os.curdir, "txt", str(random.randint(0, 1000000000000000000)) + ".txt")} '
                 f'/-Y')
