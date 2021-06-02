67da655fc6da30f91151f25f6ba04bfe66e5f3f0
kiwi-1919 <HIn@zh.email.cn>
2021-06-02 10:49:33 +0800

    test

--- compare.py
+++ compare.py
@@ -1,6 +1,7 @@
 import sqlite3
 import hashlib
 import tqdm
+import struct
 
 
 def compare():
@@ -8,14 +9,14 @@ def compare():
     cur = connect.cursor()
     content = cur.execute('SELECT sents,id from st')
     for each in tqdm.tqdm(content):
-        value = hashlib.sha1(bytes(bin(int(each[0])), 'utf-8'))
+        value = hashlib.sha1(struct.pack('f', each[0]))
         if f'{value}' == each[1]:
             pass
         else:
             raise Exception('A difference has been found.')
     content = cur.execute('SELECT wordlist,id from sw')
     for each in tqdm.tqdm(content):
-        value = hashlib.sha1(bytes(bin(int(each[0])), 'utf-8'))
+        value = hashlib.sha1(struct.pack('f', each[0]))
         if f'{value}' == each[1]:
             pass
         else:
