70b8acb6e8875d9729212a44de4ecd3ce2797eed
kiwi-1919 <HIn@zh.email.cn>
2021-06-02 10:25:38 +0800

    test

--- compare.py
+++ compare.py
@@ -8,14 +8,14 @@ def compare():
     cur = connect.cursor()
     content = cur.execute('SELECT sents,id from st')
     for each in tqdm.tqdm(content):
-        value = hashlib.sha1(bytes(bin(each[0])))
+        value = hashlib.sha1(bytes(bin(int(each[0]))))
         if f'{value}' == each[1]:
             pass
         else:
             raise Exception('A difference has been found.')
     content = cur.execute('SELECT wordlist,id from sw')
     for each in tqdm.tqdm(content):
-        value = hashlib.sha1(bytes(bin(each[0])))
+        value = hashlib.sha1(bytes(bin(int(each[0]))))
         if f'{value}' == each[1]:
             pass
         else:
