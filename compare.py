be922b900ab519bb7273c778d4c7e318eac4e9b5
kiwi-1919 <HIn@zh.email.cn>
2021-06-02 12:31:03 +0800

    test

--- compare.py
+++ compare.py
@@ -9,6 +9,8 @@ def compare():
     cur = connect.cursor()
     content = cur.execute('SELECT sents,id from st')
     for each in tqdm.tqdm(content):
+        if not each:
+            continue
         value = hashlib.sha1(bytes(bin(eval(each[0])), 'utf-8'))
         if f'{value}' == each[1]:
             pass
@@ -16,11 +18,14 @@ def compare():
             raise Exception('A difference has been found.')
     content = cur.execute('SELECT wordlist,id from sw')
     for each in tqdm.tqdm(content):
+        if not each:
+            continue
         value = hashlib.sha1(bytes(bin(eval(each[0])), 'utf-8'))
         if f'{value}' == each[1]:
             pass
         else:
             raise Exception('A difference has been found')
+    connect.close()
 
 
 if __name__ == '__main__':
