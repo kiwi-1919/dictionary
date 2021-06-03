ee5d61f05f53df0017f2f5ce2f7c3d0afd42152e
kiwi-1919 <HIn@zh.email.cn>
2021-06-04 00:50:01 +0800

    test

--- compare.py
+++ compare.py
@@ -3,27 +3,27 @@ import tqdm
 import dataframe
 
 
-
-
 def compare():
     connect = sqlite3.connect('.\\df\\data.db')
     cur = connect.cursor()
-    li = list(cur.execute("SELECT sents,id from st"))
-    ls = list(cur.execute("SELECT wordlist,id from sw"))
-    connect.close()
+    li = cur.execute("SELECT sents,id from st")
+    ls = cur.execute("SELECT wordlist,id from sw")
     print("sentence")
     for each in tqdm.tqdm(li):
         if dataframe.md_5(each[0]).encode() == each[1]:
             pass
         else:
+            connect.close()
             raise Exception("different")
     print("wordlist")
     for each in tqdm.tqdm(ls):
         if dataframe.md_5(each[0]).encode() == each[1]:
             pass
         else:
+            connect.close()
             raise Exception("different")
     print("no error was found")
+    connect.close()
 
 
 if __name__ == '__main__':
