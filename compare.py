bb6e6e7a7bfa103ed38cfe1d60b74c9dfd4de375
kiwi-1919 <HIn@zh.email.cn>
2021-06-04 00:31:59 +0800

    test

---
+++ compare.py
@@ -0,0 +1,28 @@
+import sqlite3
+import tqdm
+import dataframe
+
+
+def compare():
+    connect = sqlite3.connect('.\\df\\data.db')
+    cur = connect.cursor()
+    li = cur.execute("SELECT sents,id from st")
+    ls = cur.execute("SELECT wordlist,id from sw")
+    connect.close()
+    print("sentence")
+    for each in tqdm.tqdm(li):
+        if dataframe.md_5(each[0]).encode() == each[1]:
+            pass
+        else:
+            raise Exception("different")
+    print("wordlist")
+    for each in tqdm.tqdm(ls):
+        if dataframe.md_5(each[0]).encode() == each[1]:
+            pass
+        else:
+            raise Exception("different")
+    print("no error was found")
+
+
+if __name__ == '__main__':
+    compare()
