3f1bb7dda11d19abda5ec1dea1211d637365395f
kiwi-1919 <HIn@zh.email.cn>
2021-06-04 19:53:30 +0800

    test

--- run.py
+++ run.py
@@ -12,9 +12,15 @@ match:3''')
     if string == '1':
         os.system(".\\setup_dataframe.bat")
     elif string == '2':
-        os.system(".\\compare.bat")
+        if not os.listdir('.\\txt'):
+            os.system(".\\compare.bat")
+        else:
+            print("there is nothing")
     elif string == "3":
-        os.system(".\\match.bat")
+        if not os.listdir('.\\txt'):
+            os.system(".\\match.bat")
+        else:
+            print("there is nothing")
     else:
         print("again")
         os.system("pause")
