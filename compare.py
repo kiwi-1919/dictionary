4dfc98569ecfd78856dabb616dc7ca9db9c3023e
kiwi-1919 <HIn@zh.email.cn>
2021-06-04 17:07:02 +0800

    test

--- compare.py
+++ compare.py
@@ -6,7 +6,7 @@ import dataframe
 def generate(li):
     ls = [each for each in li]
     print(len(ls))
-    for item in ls:
+    for item in tqdm.tqdm(ls):
         yield item
 
 
@@ -15,7 +15,7 @@ def compare():
     li = generate(connect.execute("SELECT sents,id from st"))
     ls = generate(connect.execute("SELECT wordlist,id from sw"))
     print("begin")
-    for each in tqdm.tqdm(zip(li, ls)):
+    for each in zip(li, ls):
         if dataframe.md_5(each[0][0]).encode() == each[0][1] and dataframe.md_5(each[1][0]).encode() == each[1][1]:
             pass
         else:
