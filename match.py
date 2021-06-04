3a9bb422139922c1a2d298dff24687365cd1498d
kiwi-1919 <HIn@zh.email.cn>
2021-06-04 18:15:28 +0800

    test

--- match.py
+++ match.py
@@ -10,18 +10,18 @@ def match(string=None):
     ls = connect.execute("SELECT word from aw")
     lt = list(connect.execute("SELECT ind,sents from st"))
     for each in ls:
-        if each[0].decode() == word:
+        if each[0] == word:
             print("exist")
             break
         else:
-            print("nothing")
+            print("nothing", end=";")
     index = list()
     for each in li:
         wordlist = each[1].decode()
         if wordlist.find(word) != -1:
             index.append(each[0])
         else:
-            print("nothing")
+            print("nothing", end=";")
     for each in index:
         yield lt[each - 1]
     connect.close()
@@ -33,7 +33,11 @@ if __name__ == '__main__':
     except:
         raise Exception("???")
     try:
-        for i in range(5):
-            print(l[i])
+        n = 0
+        for each in l:
+            print(each)
+            n += 1
+            if n >= 5:
+                break
     except:
         pass
