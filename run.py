81e98b787767f05d2c097e1c506dc0d09c14a821
kiwi-1919 <HIn@zh.email.cn>
2021-06-04 18:32:53 +0800

    test

---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+
+if __name__ == '__main__':
+    string = input('''setup:1
+    compare:2
+    match:3''')
+    if string == '1':
+        os.system("setup_dataframe.bat")
+    elif string == '2':
+        os.system("compare.bat")
+    elif string == "3":
+        os.system("match.bat")
+    else:
+        print("again")
+        os.system("pause")
+    os.system("python -m run.py")
