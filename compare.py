2f502a8395fdaa007037164022f981bf838a1e2b
kiwi-1919 <HIn@zh.email.cn>
2021-06-04 16:55:25 +0800

    test

--- compare.py
+++ compare.py
@@ -3,11 +3,18 @@ import tqdm
 import dataframe
 
 
+def generate(li):
+    ls = [each for each in li]
+    print(len(ls))
+    for item in ls:
+        yield item
+
+
 def compare():
     connect = sqlite3.connect('.\\df\\data.db')
     cur = connect.cursor()
-    li = cur.execute("SELECT sents,id from st")
-    ls = cur.execute("SELECT wordlist,id from sw")
+    li = generate(cur.execute("SELECT sents,id from st"))
+    ls = generate(cur.execute("SELECT wordlist,id from sw"))
     print("begin")
     for each in tqdm.tqdm(zip(li, ls)):
         if dataframe.md_5(each[0][0]).encode() == each[0][1] and dataframe.md_5(each[1][0]).encode() == each[1][1]:
