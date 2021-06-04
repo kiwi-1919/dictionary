b2ec8ecad3ed646d2693bffc46dfa722fa7b784f
kiwi-1919 <HIn@zh.email.cn>
2021-06-04 22:45:25 +0800

    test

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
+import add_txt
+import sys
 
 os.chdir(os.curdir)
 
@@ -11,9 +13,13 @@ def run():
         pass
     string = input('''setup:1
 compare:2
-match:3''')
+match:3
+exit:4''')
     if string == '1':
-        os.system(".\\setup_dataframe.bat")
+        if os.listdir('.\\txt'):
+            os.system(".\\setup_dataframe.bat")
+        else:
+            add_txt.add()
     elif string == '2':
         if os.listdir('.\\txt'):
             os.system(".\\compare.bat")
@@ -24,6 +30,8 @@ match:3''')
             os.system(".\\match.bat")
         else:
             print("there is nothing")
+    elif string == '4':
+        sys.exit()
     else:
         print("again")
         os.system("pause")
